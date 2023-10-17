7477b2f30dd1c588f48ad65ffff890ee7fea335b
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-10-16 21:03:17 -0300

    mapeando as classes de dto e estrutura das pastas

---
+++ src/main/java/io/github/brunoyillli/productvalidationservice/core/dto/OrderProducts.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.productvalidationservice.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderProducts {
+    private Product product;
+    private int quantity;
+}
---
+++ src/main/java/io/github/brunoyillli/productvalidationservice/core/enums/ESagaStatus.java
@@ -0,0 +1,7 @@
+package io.github.brunoyillli.productvalidationservice.core.enums;
+
+public enum ESagaStatus {
+    SUCCESS,
+    ROLLBACK_PENDING,
+    FAIL
+}
